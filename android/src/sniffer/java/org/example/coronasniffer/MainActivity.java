8a9e33e2a5647d4bc4c0e2bd04f40c255e46db96
Otto Seiskari <otto.seiskari@gmail.com>
2020-08-31 22:21:33 +0300

    Display number of nearby GAEN transmitters on screen in sniffer mode

--- android/src/sniffer/java/org/example/coronasniffer/MainActivity.java
+++ android/src/sniffer/java/org/example/coronasniffer/MainActivity.java
@@ -1,9 +1,11 @@
 package org.example.coronasniffer;
 
+import android.annotation.SuppressLint;
 import android.app.Activity;
 import android.os.Bundle;
 import android.os.RemoteException;
 import android.util.Log;
+import android.widget.TextView;
 
 import org.altbeacon.beacon.Beacon;
 import org.altbeacon.beacon.BeaconConsumer;
@@ -23,10 +25,31 @@ public class MainActivity extends Activity implements BeaconConsumer {
             = "s:0-1=fd6f,i:2-17,d:18-21"; // GAEN with AEM as the "d" field
 
     private BeaconManager beaconManager;
+    private TextView countView;
+    private Region region = new Region("dummy-id", null, null, null);
+
+    private RangeNotifier rangeNotifier = new RangeNotifier() {
+        @SuppressLint("DefaultLocale")
+        @Override
+        public void didRangeBeaconsInRegion(Collection<Beacon> beacons, Region region) {
+            Log.v(TAG, "didRangeBeaconsInRegion count: " + beacons.size());
+            countView.setText(String.format("%d", beacons.size()));
+            for (Beacon b : beacons) {
+                Log.d(TAG, String.format("GAEN RPI %s, AEM %04x, RSSI %d (mean %g, n obs %d)",
+                        b.getId1(),
+                        b.getDataFields().get(0),
+                        b.getRssi(),
+                        b.getRunningAverageRssi(),
+                        b.getMeasurementCount()));
+            }
+        }
+    };
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        countView = findViewById(R.id.count_view);
     }
 
     @Override
@@ -44,7 +67,15 @@ public class MainActivity extends Activity implements BeaconConsumer {
     @Override
     protected void onPause() {
         super.onPause();
-        if (beaconManager != null) beaconManager.unbind(this);
+        if (beaconManager != null) {
+            try {
+                beaconManager.stopMonitoringBeaconsInRegion(region);
+            } catch (RemoteException e) {
+                Log.w(TAG, "Failed to stop scanning", e);
+            }
+            beaconManager.removeRangeNotifier(rangeNotifier);
+            beaconManager.unbind(this);
+        }
     }
 
     @Override
@@ -61,31 +92,17 @@ public class MainActivity extends Activity implements BeaconConsumer {
         beaconManager = org.altbeacon.beacon.BeaconManager.getInstanceForApplication(this);
         beaconManager.getBeaconParsers().clear();
         beaconManager.getBeaconParsers().add(new BeaconParser().setBeaconLayout(BEACON_LAYOUT));
-        BeaconManager.setDebug(true);
+        // BeaconManager.setDebug(true);
     }
 
     @Override
     public void onBeaconServiceConnect() {
-        RangeNotifier rangeNotifier = new RangeNotifier() {
-            @Override
-            public void didRangeBeaconsInRegion(Collection<Beacon> beacons, Region region) {
-                Log.v(TAG, "didRangeBeaconsInRegion count: " + beacons.size());
-                for (Beacon b : beacons) {
-                    Log.d(TAG, String.format("GAEN RPI %s, AEM %04x, RSSI %d (mean %g, n obs %d)",
-                            b.getId1(),
-                            b.getDataFields().get(0),
-                            b.getRssi(),
-                            b.getRunningAverageRssi(),
-                            b.getMeasurementCount()));
-                }
-            }
-        };
         try {
             Log.d(TAG, "Starting AltBeacon ranging");
-            beaconManager.startRangingBeaconsInRegion(new Region("dummy-id", null, null, null));
+            beaconManager.startRangingBeaconsInRegion(region);
             beaconManager.addRangeNotifier(rangeNotifier);
         } catch (RemoteException e) {
-            throw new RuntimeException(e);
+            Log.e(TAG, "Failed to start scanning", e);
         }
     }
 }
